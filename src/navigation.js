8d35a8d2939e89681d5fc1f219c45c9c5128b450
nhavantuonglai <info@nhavantuonglai.com>
2024-06-24 08:31:24 +0700

    update v05.01.02

---
+++ src/navigation.js
@@ -0,0 +1,81 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+	links: [
+		{
+			text: 'Giới thiệu',
+			href: 'https://nhavantuonglai.com/about',
+		},
+		{
+			text: 'Bài viết',
+			href: 'https://nhavantuonglai.com/article',
+		},
+		{
+			text: 'Dịch vụ',
+			href: 'https://nhavantuonglai.com/services',
+		},
+		{
+			text: 'Liên hệ',
+			href: 'https://nhavantuonglai.com/contact',
+		},
+	],
+	actions: [{ text: 'Đọc thêm', href: 'https://info.nhavantuonglai.com/instagram', target: '_blank' }],
+};
+
+export const footerData = {
+	links: [
+		{
+			title: 'Bài viết',
+			links: [
+				{ text: 'Viết lách', href: 'https://nhavantuonglai.com/tags/viet-lach' },
+				{ text: 'Viết thuê', href: 'https://nhavantuonglai.com/tags/viet-thue' },
+				{ text: 'Thư viện', href: 'https://nhavantuonglai.com/tags/thu-vien' },
+				{ text: 'Tôn giáo', href: 'https://nhavantuonglai.com/tags/ton-giao' },
+				{ text: 'Conan', href: 'https://www.nhavantuonglai.com/tags/conan' },
+				{ text: 'SEO', href: 'https://nhavantuonglai.com/tags/seo' },
+				{ text: 'Developer', href: 'https://nhavantuonglai.com/tags/developer' },
+			],
+		},
+		{
+			title: 'Tác giả',
+			links: [
+				{ text: 'Nguyễn Kỳ Lân', href: 'https://nhavantuonglai.com/tags/instagram' },
+				{ text: 'Thích Nhất Hạnh', href: 'https://nhavantuonglai.com/tags/thich-nhat-hanh' },
+				{ text: 'Clifford Stoll', href: 'https://nhavantuonglai.com/tags/clifford-stoll' },
+				{ text: 'Haruki Murakami', href: 'https://nhavantuonglai.com/tags/haruki-murakami' },
+				{ text: 'Gosho Aoyama', href: 'https://www.nhavantuonglai.com/tags/gosho-aoyama' },
+				{ text: 'Rachel Carson', href: 'https://nhavantuonglai.com/tags/rachel-carson' },
+			],
+		},
+		{
+			title: 'Hỗ trợ',
+			links: [
+				{ text: 'Fanpage', href: 'https://info.nhavantuonglai.com/fanpage' },
+				{ text: 'Facebook', href: 'https://facebook.com/nhavantuonglai/' },
+				{ text: 'Linkedin', href: 'https://info.nhavantuonglai.com/linkedin' },
+				{ text: 'Twitter', href: 'https://info.nhavantuonglai.com/twitter' },
+			],
+		},
+		{
+			title: 'Sản phẩm',
+			links: [
+				{ text: 'Adobe Stock', href: 'https://info.nhavantuonglai.com/stock' },
+				{ text: '500px', href: 'https://info.nhavantuonglai.com/500px' },
+				{ text: 'Freepik', href: 'https://info.nhavantuonglai.com/freepik' },
+				{ text: 'Shutterstock', href: 'https://info.nhavantuonglai.com/shutterstock' },
+				{ text: 'Flickr', href: 'https://info.nhavantuonglai.com/flickr' },
+			],
+		},
+	],
+	secondaryLinks: [
+		{ text: 'Điều khoản', href: 'https://nhavantuonglai.com/term' },
+		{ text: 'Chính sách', href: 'https://nhavantuonglai.com/policy' },
+		{ text: 'v04.01.10', href: 'https://nhavantuonglai.com/version' },
+	],
+	socialLinks: [
+		{ ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://info.nhavantuonglai.com/twitter' },
+		{ ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://info.nhavantuonglai.com/instagram' },
+		{ ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://info.nhavantuonglai.com/linkedin' },
+		{ ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+	],
+};
---
+++ src/utils/blog.ts
@@ -0,0 +1,249 @@
+import type { PaginateFunction } from 'astro';
+import { getCollection } from 'astro:content';
+import type { CollectionEntry } from 'astro:content';
+import type { Post } from '~/types';
+import { APP_BLOG } from 'nhavantuonglai:config';
+import { cleanSlug, trimSlash, BLOG_BASE, POST_PERMALINK_PATTERN, CATEGORY_BASE, TAG_BASE } from './permalinks';
+
+const generatePermalink = async ({
+	id,
+	slug,
+	pubDatetime,
+	category,
+}: {
+	id: string;
+	slug: string;
+	pubDatetime: Date;
+	category: string | undefined;
+}) =>{
+	const year = String(pubDatetime.getFullYear()).padStart(4, '0');
+	const month = String(pubDatetime.getMonth() + 1).padStart(2, '0');
+	const day = String(pubDatetime.getDate()).padStart(2, '0');
+	const hour = String(pubDatetime.getHours()).padStart(2, '0');
+	const minute = String(pubDatetime.getMinutes()).padStart(2, '0');
+	const second = String(pubDatetime.getSeconds()).padStart(2, '0');
+
+	const permalink = POST_PERMALINK_PATTERN.replace('%slug%', slug)
+		.replace('%id%', id)
+		.replace('%category%', category || '')
+		.replace('%year%', year)
+		.replace('%month%', month)
+		.replace('%day%', day)
+		.replace('%hour%', hour)
+		.replace('%minute%', minute)
+		.replace('%second%', second);
+
+	return permalink
+		.split('/')
+		.map((el) =>trimSlash(el))
+		.filter((el) =>!!el)
+		.join('/');
+};
+
+const getNormalizedPost = async (post: CollectionEntry<'post'>): Promise<Post>=>{
+	const { id, slug: rawSlug = '', data } = post;
+	const { Content, remarkPluginFrontmatter } = await post.render();
+
+	const {
+		pubDatetime: rawpubDatetime = new Date(),
+		updateDate: rawUpdateDate,
+		title,
+		description,
+		image,
+		tags: rawTags = [],
+		category: rawCategory,
+		author,
+		draft = false,
+		metadata = {},
+	} = data;
+
+	const slug = cleanSlug(rawSlug); // cleanSlug(rawSlug.split('/').pop());
+	const pubDatetime = new Date(rawpubDatetime);
+	const updateDate = rawUpdateDate ? new Date(rawUpdateDate) : undefined;
+	const category = rawCategory ? cleanSlug(rawCategory) : undefined;
+	const tags = rawTags.map((tag: string) =>cleanSlug(tag));
+
+	return {
+		id: id,
+		slug: slug,
+		permalink: await generatePermalink({ id, slug, pubDatetime, category }),
+
+		pubDatetime: pubDatetime,
+		updateDate: updateDate,
+
+		title: title,
+		description: description,
+		image: image,
+
+		category: category,
+		tags: tags,
+		author: author,
+
+		draft: draft,
+
+		metadata,
+
+		Content: Content,
+
+		readingTime: remarkPluginFrontmatter?.readingTime,
+	};
+};
+
+const getRandomizedPosts = (array: Post[], num: number) =>{
+	const newArray: Post[] = [];
+
+	while (newArray.length < num && array.length >0) {
+		const randomIndex = Math.floor(Math.random() * array.length);
+		newArray.push(array[randomIndex]);
+		array.splice(randomIndex, 1);
+	}
+
+	return newArray;
+};
+
+const load = async function (): Promise<Array<Post>>{
+	const posts = await getCollection('post');
+	const normalizedPosts = posts.map(async (post) =>await getNormalizedPost(post));
+
+	const results = (await Promise.all(normalizedPosts))
+		.sort((a, b) =>b.pubDatetime.valueOf() - a.pubDatetime.valueOf())
+		.filter((post) =>!post.draft);
+
+	return results;
+};
+
+let _posts: Array<Post>;
+
+export const isBlogEnabled = APP_BLOG.isEnabled;
+export const isRelatedPostsEnabled = APP_BLOG.isRelatedPostsEnabled;
+export const isBlogListRouteEnabled = APP_BLOG.list.isEnabled;
+export const isBlogPostRouteEnabled = APP_BLOG.post.isEnabled;
+export const isBlogCategoryRouteEnabled = APP_BLOG.category.isEnabled;
+export const isBlogTagRouteEnabled = APP_BLOG.tag.isEnabled;
+
+export const blogListRobots = APP_BLOG.list.robots;
+export const blogPostRobots = APP_BLOG.post.robots;
+export const blogCategoryRobots = APP_BLOG.category.robots;
+export const blogTagRobots = APP_BLOG.tag.robots;
+
+export const blogPostsPerPage = APP_BLOG?.postsPerPage;
+
+export const fetchPosts = async (): Promise<Array<Post>>=>{
+	if (!_posts) {
+		_posts = await load();
+	}
+
+	return _posts;
+};
+
+export const findPostsBySlugs = async (slugs: Array<string>): Promise<Array<Post>>=>{
+	if (!Array.isArray(slugs)) return [];
+
+	const posts = await fetchPosts();
+
+	return slugs.reduce(function (r: Array<Post>, slug: string) {
+		posts.some(function (post: Post) {
+			return slug === post.slug && r.push(post);
+		});
+		return r;
+	}, []);
+};
+
+export const findPostsByIds = async (ids: Array<string>): Promise<Array<Post>>=>{
+	if (!Array.isArray(ids)) return [];
+
+	const posts = await fetchPosts();
+
+	return ids.reduce(function (r: Array<Post>, id: string) {
+		posts.some(function (post: Post) {
+			return id === post.id && r.push(post);
+		});
+		return r;
+	}, []);
+};
+
+export const findLatestPosts = async ({ count }: { count?: number }): Promise<Array<Post>>=>{
+	const _count = count || 4;
+	const posts = await fetchPosts();
+
+	return posts ? posts.slice(0, _count) : [];
+};
+
+export const getStaticPathsBlogList = async ({ paginate }: { paginate: PaginateFunction }) =>{
+	if (!isBlogEnabled || !isBlogListRouteEnabled) return [];
+	return paginate(await fetchPosts(), {
+		params: { blog: BLOG_BASE || undefined },
+		pageSize: blogPostsPerPage,
+	});
+};
+
+export const getStaticPathsBlogPost = async () =>{
+	if (!isBlogEnabled || !isBlogPostRouteEnabled) return [];
+	return (await fetchPosts()).flatMap((post) =>({
+		params: {
+			blog: post.permalink,
+		},
+		props: { post },
+	}));
+};
+
+export const getStaticPathsBlogCategory = async ({ paginate }: { paginate: PaginateFunction }) =>{
+	if (!isBlogEnabled || !isBlogCategoryRouteEnabled) return [];
+
+	const posts = await fetchPosts();
+	const categories = new Set<string>();
+	posts.map((post) =>{
+		typeof post.category === 'string' && categories.add(post.category.toLowerCase());
+	});
+
+	return Array.from(categories).flatMap((category) =>
+		paginate(
+			posts.filter((post) =>typeof post.category === 'string' && category === post.category.toLowerCase()),
+			{
+				params: { category: category, blog: CATEGORY_BASE || undefined },
+				pageSize: blogPostsPerPage,
+				props: { category },
+			}
+		)
+	);
+};
+
+export const getStaticPathsBlogTag = async ({ paginate }: { paginate: PaginateFunction }) =>{
+	if (!isBlogEnabled || !isBlogTagRouteEnabled) return [];
+
+	const posts = await fetchPosts();
+	const tags = new Set<string>();
+	posts.map((post) =>{
+		Array.isArray(post.tags) && post.tags.map((tag) =>tags.add(tag.toLowerCase()));
+	});
+
+	return Array.from(tags).flatMap((tag) =>
+		paginate(
+			posts.filter((post) =>Array.isArray(post.tags) && post.tags.find((elem) =>elem.toLowerCase() === tag)),
+			{
+				params: { tag: tag, blog: TAG_BASE || undefined },
+				pageSize: blogPostsPerPage,
+				props: { tag },
+			}
+		)
+	);
+};
+
+export function getRelatedPosts(allPosts: Post[], currentSlug: string, currentTags: string[]) {
+	if (!isBlogEnabled || !isRelatedPostsEnabled) return [];
+
+	const relatedPosts = getRandomizedPosts(
+		allPosts.filter((post) =>post.slug !== currentSlug && post.tags?.some((tag) =>currentTags.includes(tag))),
+		APP_BLOG.relatedPostsCount
+	);
+
+	if (relatedPosts.length < APP_BLOG.relatedPostsCount) {
+		const morePosts = getRandomizedPosts(
+			allPosts.filter((post) =>post.slug !== currentSlug && !post.tags?.some((tag) =>currentTags.includes(tag))),
+			APP_BLOG.relatedPostsCount - relatedPosts.length
+		);
+		relatedPosts.push(...morePosts);
+	}
+
+	return relatedPosts;
+}
