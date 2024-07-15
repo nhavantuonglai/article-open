8bb4bf4d63cda5936a2e5c90253188045c39ae9c
nhavantuonglai <info@nhavantuonglai.com>
2024-07-15 14:36:36 +0700

    up

--- src/navigation.js
+++ src/navigation.js
@@ -1,7 +1,6 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
-
 	links: [
 		{
 			text: 'Giới thiệu',
@@ -20,12 +19,7 @@ export const headerData = {
 			href: 'https://nhavantuonglai.com/contact',
 		},
 	],
-
-	actions: [{
-		text: 'Đọc thêm',
-		href: 'https://info.nhavantuonglai.com/instagram',
-		target: '_blank'
-		}],
+	actions: [{ text: 'Đọc thêm', href: 'https://info.nhavantuonglai.com/instagram', target: '_blank'}],
 };
 
 export const footerData = {
@@ -33,6 +27,7 @@ export const footerData = {
 		{
 			title: 'Danh mục',
 			links: [
+<<<<<<< HEAD
 				{
 					text: 'Viết lách',
 					href: 'https://info.nhavantuonglai.com/viet-lach'
@@ -70,11 +65,20 @@ export const footerData = {
 					text: 'Kỹ năng viết',
 					href: 'https://nhavantuonglai.com/tags/gosho-aoyama'
 				},
+=======
+				{ text: 'Viết lách', href: 'https://info.nhavantuonglai.com/viet-lach'},
+				{ text: 'Viết thuê', href: 'https://info.nhavantuonglai.com/viet-thue'},
+				{ text: 'Thư viện', href: 'https://info.nhavantuonglai.com/thu-vien' },
+				{ text: 'Tôn giáo', href: 'https://nhavantuonglai.com/tags/ton-giao' },
+				{ text: 'SEO', href: 'https://nhavantuonglai.com/tags/seo' },
+				{ text: 'Lập trình', href: 'https://info.nhavantuonglai.com/developer'},
+>>>>>>> parent of 59314f4c (update)
 			],
 		},
 		{
 			title: 'Tác giả',
 			links: [
+<<<<<<< HEAD
 				{
 					text: 'Nguyễn Kỳ Lân',
 					href: 'https://nhavantuonglai.com/tags/instagram'
@@ -91,11 +95,20 @@ export const footerData = {
 					text: 'Haruki Murakami',
 					href: 'https://nhavantuonglai.com/tags/haruki-murakami'
 				},
+=======
+				{ text: 'Nguyễn Kỳ Lân', href: 'https://nhavantuonglai.com/tags/instagram' },
+				{ text: 'Thích Nhất Hạnh', href: 'https://nhavantuonglai.com/tags/thich-nhat-hanh' },
+				{ text: 'Clifford Stoll', href: 'https://nhavantuonglai.com/tags/clifford-stoll' },
+				{ text: 'Haruki Murakami', href: 'https://nhavantuonglai.com/tags/haruki-murakami' },
+				{ text: 'Gosho Aoyama', href: 'https://nhavantuonglai.com/tags/gosho-aoyama' },
+				{ text: 'Rachel Carson', href: 'https://nhavantuonglai.com/tags/rachel-carson' },
+>>>>>>> parent of 59314f4c (update)
 			],
 		},
 		{
 			title: 'Tác phẩm',
 			links: [
+<<<<<<< HEAD
 				{
 					text: 'Đường xưa mây trắng',
 					href: 'https://nhavantuonglai.com/tags/duong-xua-may-trang'
@@ -112,43 +125,32 @@ export const footerData = {
 					text: 'Thần điêu hiệp lữ',
 					href: 'https://nhavantuonglai.com/tags/than-dieu-hiep-lu'
 				},
+=======
+				{ text: 'Fanpage', href: 'https://info.nhavantuonglai.com/fanpage' },
+				{ text: 'Facebook', href: 'https://info.nhavantuonglai.com/facebook' },
+			],
+		},
+		{
+			title: 'Tài nguyên',
+			links: [
+				{ text: 'Adobe Stock', href: 'https://info.nhavantuonglai.com/stock' },
+				{ text: '500px', href: 'https://info.nhavantuonglai.com/500px' },
+				{ text: 'Freepik', href: 'https://info.nhavantuonglai.com/freepik' },
+				{ text: 'Shutterstock', href: 'https://info.nhavantuonglai.com/shutterstock' },
+				{ text: 'Flickr', href: 'https://info.nhavantuonglai.com/flickr' },
+>>>>>>> parent of 59314f4c (update)
 			],
 		},
 	],
 	secondaryLinks: [
-		{
-			text: 'Điều khoản',
-			href: 'https://nhavantuonglai.com/term'
-		},
-		{
-			text: 'Chính sách',
-			href: 'https://nhavantuonglai.com/policy'
-		},
-		{
-			text: 'Cập nhật',
-			href: 'https://nhavantuonglai.com/version'
-		},
+		{ text: 'Điều khoản', href: 'https://nhavantuonglai.com/term' },
+		{ text: 'Chính sách', href: 'https://nhavantuonglai.com/policy' },
+		{ text: 'Cập nhật', href: 'https://nhavantuonglai.com/version' },
 	],
 	socialLinks: [
-		{
-			ariaLabel: 'X',
-			icon: 'tabler:brand-x',
-			href: 'https://info.nhavantuonglai.com/twitter'
-		},
-		{
-			ariaLabel: 'Instagram',
-			icon: 'tabler:brand-instagram',
-			href: 'https://info.nhavantuonglai.com/instagram'
-		},
-		{
-			ariaLabel: 'Linkedin',
-			icon: 'tabler:brand-linkedin',
-			href: 'https://info.nhavantuonglai.com/linkedin'
-		},
-		{
-			ariaLabel: 'RSS',
-			icon: 'tabler:rss',
-			href: getAsset('/rss.xml')
-		},
+		{ ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://info.nhavantuonglai.com/twitter' },
+		{ ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://info.nhavantuonglai.com/instagram' },
+		{ ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://info.nhavantuonglai.com/linkedin' },
+		{ ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
 	],
 };
