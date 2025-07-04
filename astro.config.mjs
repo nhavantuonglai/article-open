3489776bf6022b363ffcbfa658cadbeec718e361
nhavantuonglai <info@nhavantuonglai.com>
2025-07-04 10:27:25 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,67 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
+import astrowind from './src/integration';
+
+import { 
+	readingTimeRemarkPlugin, 
+	responsiveTablesRehypePlugin, 
+	lazyImagesRehypePlugin 
+} from './src/utils/frontmatter.mjs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const hasExternalScripts = false;
+const whenExternalScripts = (items = []) =>
+	hasExternalScripts
+		? Array.isArray(items)
+			? items.map((item) => item())
+			: [items()]
+		: [];
 
 export default defineConfig({
-  output: 'static',
-  integrations: [react(), tailwind({ applyBaseStyles: false }), sitemap(), mdx()],
+
+	output: 'static',
+
+	integrations: [
+
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+
+		sitemap(),
+
+		mdx(),
+
+		...whenExternalScripts(() =>
+			partytown({
+				config: { forward: ['dataLayer.push'] },
+			})
+		),
+
+		astrowind(),
+	],
+
+	markdown: {
+		remarkPlugins: [readingTimeRemarkPlugin],
+		rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
+	},
+
+	vite: {
+
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+			extensions: ['.js', '.ts']
+		},
+
+	},
+
 });
